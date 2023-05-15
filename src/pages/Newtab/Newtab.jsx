970a0a52e65ee89fa1c0da0aa782861350e2bf36
mk <v.a.stekolschikov@gmail.com>
2023-05-15 16:15:14 +0300

    clear html

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
 import './Newtab.css';
-import './Newtab.scss';
 
 const Newtab = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          !!Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload11.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
-      </header>
+    <div className="title">
+      12
     </div>
   );
 };
