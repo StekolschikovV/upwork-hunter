0a986c52c6b554d4b5f355ff639e3c57dc7b9b02
mk <v.a.stekolschikov@gmail.com>
2023-05-15 17:55:20 +0300

    added html

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import './Newtab.css';
+import './Newtab.scss';
 
 const Newtab = () => {
-  return (
-    <div className="title">
-      12
+  return (<>
+    <div className="container">
+      <header>
+        <div className="header-title">UPWORK HUNTER</div>
+      </header>
+      <div className="content">
+        <ul className="menu-container">
+          <li className="menu-active-element">Feed</li>
+          <li>Cover Letter</li>
+          <li>ToDo</li>
+          <li>Notes</li>
+          <li>Settings</li>
+        </ul>
+        <div className="content-container">123</div>
+      </div>
     </div>
-  );
+  </>);
 };
 
 export default Newtab;
