f80ea8b5627347e7c2c3c6f5531657ffea8aa2a3
mk <v.a.stekolschikov@gmail.com>
2023-05-17 15:16:51 +0300

    added error

--- src/pages/Newtab/screens/Feed/index.tsx
+++ src/pages/Newtab/screens/Feed/index.tsx
@@ -5,18 +5,20 @@ import Btn from "../../components/Btn";
 import ControlsContainer from "../../components/ControlsContainer";
 import Job from "../../components/Job";
 import LeftTime from "../../components/LeftTime";
-
+import styles from "./style.module.scss"
 const Feed = observer(() => {
 
   const store = useRootStore()
 
   return (<>
-    <ControlsContainer>
+    {store.feedStore.feed.length > 0 && <ControlsContainer>
       {/* <SVG type='sort' onClickHandler={() => console.log("123")} cursorPointer={true} /> */}
       <LeftTime />
       <Btn title="Refresh" onClick={e => store.feedStore.getJobs()} />
-    </ControlsContainer>
+    </ControlsContainer>}
     {store.feedStore.feed.splice(0, store.feedStore.showJobs).map((e, i) => <Job job={e} key={e.date + e.title + i} />)}
+
+    {store.feedStore.feed.length === 0 && <div className={styles.error}>There are no feeds added, please specify the feeds you need in the settings section.</div>}
   </>);
 })
 
